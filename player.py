242b4e71c4425b67df63601e9eb53f7996600929
Matyas Vegh <matyas.vegh@ericsson.com>
2018-09-21 10:01:25 +0200

    Fix access of game_state

--- player.py
+++ player.py
@@ -7,8 +7,8 @@ class Player:
     def betRequest(self, game_state):
         print('hello world')
         print('hello world stderr', file=sys.stderr)
-        current_buy_in = game_state.current_buy_in
-        me = game_state.players[game_state.in_action]
+        current_buy_in = game_state["current_buy_in"]
+        me = game_state["players"][game_state["in_action"]
         print('To call, would need to bet:', current_buy_in - me.bet)
 
         return current_buy_in - me.bet
