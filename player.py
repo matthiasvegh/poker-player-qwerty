c712e08cf4bcb3df635d54a32cd5b185ca1876c4
Matyas Vegh <matyas.vegh@ericsson.com>
2018-09-21 10:27:50 +0200

    Add test for having a pair

--- player.py
+++ player.py
@@ -26,8 +26,28 @@ class Player:
         me = game_state["players"][game_state["in_action"]]
 
         self.log(game_state)
+        if have_one_pair(game_state):
+            print("have one pair")
 
         return current_buy_in - me["bet"]
+
+    def get_my_hand(self, game_state):
+        return self.get_my_cards(game_state) + \
+                self.get_communal_cards(game_state)
+
+    def get_my_cards(self, game_state):
+        me = game_state["players"][game_state["in_action"]]
+        my_cards = me["hole_cards"]
+        return my_cards
+
+    def get_communal_cards(self, game_state):
+        return game_state["community_cards"]
+
+    def have_one_pair(self, game_state):
+        my_hand = self.get_my_hand(game_state)
+        ranks = [card["rank"] for card in my_hand]
+
+        return False
 
     def showdown(self, game_state):
         pass
