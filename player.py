4221f80a466b3a1fc97fbe41e081c99e39265af9
Matyas Vegh <matyas.vegh@ericsson.com>
2018-09-21 10:10:52 +0200

    Print correctly and correct syntax

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+
 import sys
 
 class Player:
@@ -6,12 +7,11 @@ class Player:
 
     def betRequest(self, game_state):
         print('hello world')
-        print('hello world stderr', file=sys.stderr)
+        print('hello world error', file=sys.stderr)
         current_buy_in = game_state["current_buy_in"]
-        me = game_state["players"][game_state["in_action"]
-        print('To call, would need to bet:', current_buy_in - me.bet)
+        me = game_state["players"][game_state["in_action"]]
 
-        return current_buy_in - me.bet
+        return current_buy_in - me["bet"]
 
     def showdown(self, game_state):
         pass
