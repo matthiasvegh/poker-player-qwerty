b2c84ddb50734ed23378e784324085dd160313a9
Matyas Vegh <matyas.vegh@ericsson.com>
2018-09-21 09:47:26 +0200

    Call bet instead of folding

--- player.py
+++ player.py
@@ -1,9 +1,14 @@
+from __future__ import print_function
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "Default Python calling player"
 
     def betRequest(self, game_state):
-        return 0
+        current_buy_in = game_state.current_buy_in
+        me = game_state.players[game_state.in_action]
+        print('To call, would need to bet:', current_buy_in - me.bet)
+
+        return current_buy_in - me.bet
 
     def showdown(self, game_state):
         pass
